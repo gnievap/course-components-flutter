402d40b795df2c6a5f073e7f7015b54ce18b921d
Gaby Nieva <gaby.nieva@outlook.com>
2024-04-19 14:24:30 -0600

    Creating ListTiles from a constant list

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ListView1Screen extends StatelessWidget {
   const ListView1Screen({Key? key}) : super(key: key);
 
+  final options = const ['Ginger', 'KC', 'Moji', 'Moka'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +11,19 @@ class ListView1Screen extends StatelessWidget {
         title: const Text('Listview Tipo 1'),
       ),
       body: ListView(
-        children: const [
-          Text('Baby Gings <3'),
-          Text('Baby Gings <3'),
-          Text('Baby Gings <3'),
-          Text('Baby Gings <3'),
-          Text('Baby Gings <3'),
-          Text('Baby Gings <3'),
+        children: [
+          ...options
+              .map((pet) => ListTile(
+                    leading: const Icon(Icons.pets),
+                    title: Text(pet),
+                    trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                  ))
+              .toList(),
+
+          // ListTile(
+          //   leading: Icon(Icons.pets),
+          //   title: Text('Gings'),
+          // ),
         ],
       ),
     );
