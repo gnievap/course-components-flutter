3cf2f16915519054f52ebe7c65946f1699f03898
Gaby Nieva <gaby.nieva@outlook.com>
2024-04-22 15:51:34 -0600

    Taps on ListTiles and begining with colors of the AppBar

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -9,13 +9,22 @@ class ListView2Screen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Listview Tipo 1'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
       ),
       body: ListView.separated(
         itemCount: options.length,
         itemBuilder: (context, index) => ListTile(
           leading: const Icon(Icons.pets),
           title: Text(options[index]),
-          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+          trailing: const Icon(
+            Icons.arrow_forward_ios_outlined,
+            color: Colors.indigo,
+          ),
+          onTap: () {
+            final pet = options[index];
+            print(pet);
+          },
         ),
         separatorBuilder: (_, __) => const Divider(),
       ),
